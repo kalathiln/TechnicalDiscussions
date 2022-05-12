97ae150bfab5e3187650ea4081d978b4b8f9c4a8
kalathiln <nikhkk@gmail.com>
2022-05-12 17:16:53 +0200

    Adding Test CLASS , Implementation required

---
+++ src/test/java/StreamsAPIAndLambdaTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Need to add test cases here
+ * Try to use Mockito
+ */
+class StreamsAPIAndLambdaTest {
+    StreamsAPIAndLambda streamsAPIAndLambda = new StreamsAPIAndLambda();
+
+    public StreamsAPIAndLambda getStreamsAPIAndLambda() {
+        return streamsAPIAndLambda;
+    }
+
+}
