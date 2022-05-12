489306858f849aca590bf57bbef6006617f6a45e
kalathiln <nikhkk@gmail.com>
2022-05-12 17:12:45 +0200

    Addd method to tryout summary statistics function and changed the file name to StreamsAPIAndLambda

--- src/main/java/StreamsAPIAndLambda.java
+++ src/main/java/StreamsAPIAndLambda.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class TechnicalDiscussion {
+public class StreamsAPIAndLambda {
 
     public static void main(String[] args) {
 
